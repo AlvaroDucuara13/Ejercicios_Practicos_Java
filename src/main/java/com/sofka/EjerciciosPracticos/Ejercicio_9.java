67c8c6eecf04796eeb9539e8f6e8f62d140548bb
Alvaro Ducuara <alvaro.isledier@gmail.com>
2022-02-11 21:55:57 -0500

    Se ha modificado el ejercicio 9 de las practicas de java

--- src/main/java/com/sofka/EjerciciosPracticos/Ejercicio_9.java
+++ src/main/java/com/sofka/EjerciciosPracticos/Ejercicio_9.java
@@ -1,4 +1,16 @@
 package com.sofka.EjerciciosPracticos;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Ejercicio_9 {
+    public static void main(String[] args) {
+        Scanner datoEntrada = new Scanner(System.in);
+        String fraselocal="La sonrisa sera la mejor arma contra la tristeza";
+        String fraseModificada= fraselocal.replace("a", "e");
+        System.out.println("Ingrese una frase de su comodidad: ");
+        String fraseEntrada = datoEntrada.nextLine();
+        System.out.println(fraseModificada.toUpperCase()+" "+fraseEntrada.toUpperCase());
+
+    }
 }
