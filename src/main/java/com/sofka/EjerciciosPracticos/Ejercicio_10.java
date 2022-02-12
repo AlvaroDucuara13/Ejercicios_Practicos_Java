e9254ea54b3ddae2587be61d04e18514e8852d5c
Alvaro Ducuara <alvaro.isledier@gmail.com>
2022-02-11 22:05:43 -0500

    Se ha completdo el ejercicio 10 de las practicas de java

---
+++ src/main/java/com/sofka/EjerciciosPracticos/Ejercicio_10.java
@@ -0,0 +1,15 @@
+package com.sofka.EjerciciosPracticos;
+
+import java.util.Scanner;
+
+public class Ejercicio_10 {
+
+    public static void main(String[] args) {
+        Scanner datoEntrada = new Scanner(System.in);
+        System.out.println("Introduce una frase: ");
+        String fraseEntrada = datoEntrada.nextLine();
+        String fraseModificada = fraseEntrada.replace(" ", "");
+        System.out.println(fraseModificada);
+    }
+
+}
